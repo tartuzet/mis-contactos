f10b4fbcf289d4d48b7eee24cef4f52174e8e2bb
Gustavo Isidoro <tartuzet@gmail.com>
2024-06-12 16:01:18 -0600

    se puso el fondo en notificaciones y perfil

--- src/screens/Nofitications.js
+++ src/screens/Nofitications.js
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { globalStyles } from '../styles/global'
+
 
 export default function Nofitications() {
   return (
-    <View>
+    <View style={globalStyles.screenContainer}>
       <Text>Nofitications</Text>
     </View>
   )
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { globalStyles } from '../styles/global'
+
 
 export default function Profile() {
   return (
-    <View>
+    <View style={globalStyles.screenContainer}>
       <Text>Profile</Text>
     </View>
   )
